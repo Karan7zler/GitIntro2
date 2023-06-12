2e03b4d58b652a919b74fbddfdf1e88bfbf82e3e
Karan7zler <23karan2gmail.com>
2023-06-12 13:19:42 +0530

    Details

---
+++ src/org/testing/Employee.java
@@ -0,0 +1,25 @@
+package org.testing;
+
+public class Employee {
+
+	public void empName() {
+System.out.println("karan");
+	}
+	public void empId() {
+System.out.println("4732");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		
+		e.empId();
+		e.empName();
+		
+		
+		
+		
+		
+		
+	}
+}
